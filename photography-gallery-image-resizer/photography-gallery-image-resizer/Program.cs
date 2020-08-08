9595346b9cf38806079ebb34e92ff2bb3ea3aa4b
georgesavill <gpsavill+github@gmail.com>
2020-08-08 13:51:40 +0100

    Add image resize functionality

--- photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
+++ photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace photography_gallery_image_resizer
 {
@@ -30,7 +33,40 @@ namespace photography_gallery_image_resizer
         static void ProcessImages(string inputDirectory, string outputDirectory)
         {
             string[] fileList = Directory.GetFiles(inputDirectory, "*.jpg", SearchOption.AllDirectories);
-            foreach (string entry in fileList) { Console.WriteLine(entry); }
+            foreach (string imagePath in fileList) {
+                Console.WriteLine("Resizing " + imagePath);
+                ResizeImage(imagePath, 315, "_thumbnail", inputDirectory, outputDirectory);
+                ResizeImage(imagePath, 765, "_preview", inputDirectory, outputDirectory);
+            }
+        }
+
+        static void ResizeImage(string imagePath, int newWidth, string outputType, string inputDirectory, string outputDirectory)
+        {
+            using Image image = Image.Load(imagePath);
+
+            image.Mutate(x => x.Resize(newWidth, Convert.ToInt32(newWidth * GetImageRatio(image.Width, image.Height))));
+
+            string uploadedImageFileName = imagePath.Split(".").First().Split("\\").Last();
+            string uploadedImageDirectory = GetRelativeImageDirectory(inputDirectory, imagePath);
+            string uploadedImageExtension = imagePath.Split(".").Last();
+
+            Directory.CreateDirectory(outputDirectory + uploadedImageDirectory);
+
+            image.Save(outputDirectory + uploadedImageDirectory + "//" + uploadedImageFileName + outputType + "." + uploadedImageExtension);
+        }
+
+        static string GetRelativeImageDirectory(string inputDirectory, string imagePath)
+        {
+            string[] splitImagePath = imagePath.Split(inputDirectory).Last().Split("\\");
+            Array.Resize(ref splitImagePath, splitImagePath.Length - 1);
+            return string.Join("\\", splitImagePath);
+        }
+
+        static float GetImageRatio(int width, int height)
+        {
+            float w = width;
+            float h = height;
+            return h / w;
         }
     }
 }
