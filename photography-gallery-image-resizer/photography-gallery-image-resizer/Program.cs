33fdc598e55b99d50afc12c7bade98bf344bfe92
georgesavill <gpsavill+github@gmail.com>
2020-08-08 11:58:51 +0100

    Add .net core console app template

---
+++ photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace photography_gallery_image_resizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
