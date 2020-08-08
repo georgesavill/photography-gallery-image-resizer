f1eae69d0e7b4ea56369d2820d5581b6014d98cb
georgesavill <gpsavill+github@gmail.com>
2020-08-08 12:47:48 +0100

    Add code for listing files

--- photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
+++ photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace photography_gallery_image_resizer
 {
@@ -9,10 +10,27 @@ namespace photography_gallery_image_resizer
             if (args.Length != 2)
             {
                 Console.WriteLine("Please provide two arguments - \"INPUT DIRECTORY\" \"OUTPUT DIRECTORY\"");
-                Environment.ExitCode = 0xA0;
-            } 
-            
+                Environment.ExitCode = -1;
+            }
 
+            string inputDirectory = args[0];
+            string outputDirectory = args[1];
+            Console.WriteLine("Resizing images in " + inputDirectory);
+            if (Directory.Exists(inputDirectory))
+            {
+                ProcessImages(inputDirectory, outputDirectory);
+            }
+            else
+            {
+                Console.WriteLine("Input directory provided (" + inputDirectory + ") does not exist");
+                Environment.ExitCode = -1;
+            }
+        }
+
+        static void ProcessImages(string inputDirectory, string outputDirectory)
+        {
+            string[] fileList = Directory.GetFiles(inputDirectory, "*.jpg", SearchOption.AllDirectories);
+            foreach (string entry in fileList) { Console.WriteLine(entry); }
         }
     }
 }
