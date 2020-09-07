f5b82be5c062bf150b45271a03d2c222c449bc8e
georgesavill <gpsavill+github@gmail.com>
2020-09-07 20:25:34 +0100

    Replace ImageSharp with ImageMagick
    ImageMagick allows for the creation of pjpeg and webp images

--- photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
+++ photography-gallery-image-resizer/photography-gallery-image-resizer/Program.cs
@@ -3,13 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Jpeg;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.Metadata.Profiles.Exif;
-using SixLabors.ImageSharp.Processing;
 using StackExchange.Redis;
-
+using ImageMagick;
 
 namespace photography_gallery_image_resizer
 {
@@ -66,7 +61,7 @@ namespace photography_gallery_image_resizer
                     {
                         foreach (int size in imageSizes)
                         {
-                        ResizeImage(imagePath, size, uploadedImageFileName, targetDirectory, uploadedImageExtension, redisDatabase);
+                            ResizeImage(imagePath, size, uploadedImageFileName, targetDirectory, uploadedImageExtension, redisDatabase);
                         }
                         File.Copy(imagePath, targetDirectory + directorySeparator + uploadedImageFileName + "." + uploadedImageExtension, true);
                     }
@@ -88,9 +83,9 @@ namespace photography_gallery_image_resizer
             FileInfo inputImage = new FileInfo(imagePath);
             FileInfo existingImage = new FileInfo(potentialExistingImage);
 
-            if (inputImage.Length != existingImage.Length) { 
+            if (inputImage.Length != existingImage.Length) {
                 Console.WriteLine(imagePath + " exists in output directory, but the image size has changed, resizing...");
-                return true; 
+                return true;
             }
             else if (!File.ReadAllBytes(inputImage.FullName).SequenceEqual(File.ReadAllBytes(existingImage.FullName)))
             {
@@ -104,33 +99,43 @@ namespace photography_gallery_image_resizer
 
         }
 
+
         static void ResizeImage(string imagePath, int newWidth, string uploadedImageFileName, string uploadedImageDirectory, string uploadedImageExtension, IDatabase redisDatabase)
         {
             Directory.CreateDirectory(uploadedImageDirectory);
-            using Image image = Image.Load(imagePath);
-            image.Mutate(x => x.Resize(newWidth, Convert.ToInt32(newWidth * GetImageRatio(image.Width, image.Height))));
-            int imageQuality;
-            if (newWidth < 500)
+
+            using (MagickImage image = new MagickImage(imagePath))
             {
-                imageQuality = 75;
-            } else
-            {
-                imageQuality = 65;
+                IExifProfile metadata = image.GetExifProfile();
+                redisDatabase.HashSet(uploadedImageFileName + "." + uploadedImageExtension, new HashEntry[] {
+                    new HashEntry("Model",metadata.GetValue(ExifTag.Model).ToString()),
+                    new HashEntry("LensModel",metadata.GetValue(ExifTag.LensModel).ToString()),
+                    new HashEntry("FNumber",FixFNumber(metadata.GetValue(ExifTag.FNumber).ToString())),
+                    new HashEntry("FocalLength",metadata.GetValue(ExifTag.FocalLength).ToString()),
+                    new HashEntry("ExposureTime",metadata.GetValue(ExifTag.ExposureTime).ToString()),
+                    new HashEntry("Height",image.Height),
+                    new HashEntry("Width",image.Width),
+                    new HashEntry("AspectRatio",GetImageRatio(image.Width, image.Height))
+                });
+
+                image.Resize(newWidth, Convert.ToInt32(newWidth * GetImageRatio(image.Width, image.Height)));
+                image.Strip();
+                int imageQuality;
+                if (newWidth < 500)
+                {
+                    imageQuality = 75;
+                }
+                    else
+                {
+                    imageQuality = 65;
+                }
+                image.Quality = imageQuality;
+                image.Format = MagickFormat.Pjpeg;
+
+                image.Write(uploadedImageDirectory + directorySeparator + uploadedImageFileName + "_" + newWidth.ToString() + ".jpg");
             }
-            JpegEncoder encoder = new JpegEncoder { Quality = imageQuality };
-            image.Save(uploadedImageDirectory + directorySeparator + uploadedImageFileName + "_" + newWidth.ToString() + ".jpg", encoder);
-            redisDatabase.HashSet(uploadedImageFileName + "." + uploadedImageExtension, new HashEntry[] {
-                new HashEntry("Model",image.Metadata.ExifProfile.GetValue(ExifTag.Model).ToString()),
-                new HashEntry("LensModel",image.Metadata.ExifProfile.GetValue(ExifTag.LensModel).ToString()),
-                new HashEntry("FNumber",FixFNumber(image.Metadata.ExifProfile.GetValue(ExifTag.FNumber).ToString())),
-                new HashEntry("FocalLength",image.Metadata.ExifProfile.GetValue(ExifTag.FocalLength).ToString()),
-                new HashEntry("ExposureTime",image.Metadata.ExifProfile.GetValue(ExifTag.ExposureTime).ToString()),
-                new HashEntry("Height",image.Height),
-                new HashEntry("Width",image.Width),
-                new HashEntry("AspectRatio",GetImageRatio(image.Width, image.Height))
-            });
         }
-        
+
         static string FixFNumber(string input)
         {
             string[] splitInput = input.Split("/");
